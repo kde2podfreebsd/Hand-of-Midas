dbb8586c9312a6eb1dd2ef05bb7918f71df32978
Bogomolov Ivan <qhomop@gmail.com>
2025-04-16 19:53:54 +0300

    add cors

--- llmAgents/api/main.py
+++ llmAgents/api/main.py
@@ -1,4 +1,3 @@
-from fastapi import FastAPI
 from llmAgents.api.router import (
     post_router,
     chat_router,
@@ -7,10 +6,34 @@ from llmAgents.api.router import (
     user_address_router
 )
 
-app = FastAPI()
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
-app.include_router(post_router.router)
-app.include_router(chat_router.router)
-app.include_router(investment_router.router)
-app.include_router(pools_router.router)
-app.include_router(user_address_router.router)
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="DeFI Wizzard",
+        description="API для анализа портфеля, анкетирования, новостей и чата с AI-агентом",
+        version="1.0.0"
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    app.include_router(post_router.router)
+    app.include_router(chat_router.router)
+    app.include_router(investment_router.router)
+    app.include_router(pools_router.router)
+    app.include_router(user_address_router.router)
+
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)
